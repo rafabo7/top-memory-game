763c537739e4f3fb0bd4577dc70da97f945f91ab
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-09-24 18:28:35 +0200

    feat: add pokeapi call, parse the json and set to state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
 import { Header } from "./Components/Header"
 import { Game } from "./Components/Game"
 import { Footer } from "./Components/Footer"
+import { useState, useEffect } from "react"
 
 
 function App () {
+  const [cards, setCards] = useState({})
+  const [clicked, setClicked] = useState(null)
+
+  const getPokemons = async () => {
+    const randomId = Math.floor(Math.random() * 200)
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}/`)
+    const json = await response.json()
+    const {
+      name,
+      id,
+      sprites: {
+        front_default: sprite
+      }
+    } = json
+
+    const newCard = {name, id, sprite}
+
+    setCards(newCard)
+  }
+console.log(cards);
+
+  
+
+
 
 return (
   <>
+  <button onClick={() => getPokemons()}>click</button>
+  {cards && <img src={cards.sprite} style={{width: "200px"}} alt="WTF" />}
   <Header />
   <Game />
   <Footer />
